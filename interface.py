e432230d3e592705cafb9a15a90e68312e4226d0
ManojPriyanjana <manojpriyanjana5@gmail.com>
2025-05-08 01:59:38 +0530

    Create window only

---
+++ interface.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+# Create main window only
+root = tk.Tk()
+root.title("Simple UI")
+root.geometry("600x800")  # Window size 600x800
+
+# Start the application
+root.mainloop()
