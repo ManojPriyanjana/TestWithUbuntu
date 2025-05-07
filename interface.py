efeb347603c3e36a942b4928ae2221e169f5ce94
ManojPriyanjana <manojpriyanjana5@gmail.com>
2025-05-08 02:06:17 +0530

    add forloop

--- interface.py
+++ interface.py
@@ -5,5 +5,11 @@ root = tk.Tk()
 root.title("Simple UI")
 root.geometry("600x800")  # Window size 600x800
 
+# Configure grid: 3 rows and 3 columns to equally expand
+for i in range(3):
+    root.rowconfigure(i, weight=1)
+    root.columnconfigure(i, weight=1)
+
+
 # Start the application
 root.mainloop()
